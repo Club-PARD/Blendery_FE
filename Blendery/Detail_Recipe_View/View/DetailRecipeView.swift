7e1c96de675635811a273bdb283ce389540412b5
parkyeongeon <yeongeon1003@gmail.com>
2025-12-28 22:24:04 +0900

    Make Option Button UI in DetailRecipe View

--- Blendery/Detail_Recipe_View/View/DetailRecipeView.swift
+++ Blendery/Detail_Recipe_View/View/DetailRecipeView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct DetailRecipeView: View {
+    @State private var searchText = ""
+    
     var body: some View {
         VStack{
             RecipeTitle()
                 .padding(22)
             RecipeStepList()
                 .padding(16)
+            HStack{
+                Spacer()
+                OptionButton()
+                    .padding(.trailing)
+            }
+            SearchBar(text: $searchText)
+                .padding(.horizontal, 16)
+                .padding(.vertical, )
         }
     }
 }
---
+++ Blendery/Recipe_Option_Button/Model/OptionModel.swift
@@ -0,0 +1,16 @@
+//
+//  OptionModel.swift
+//  Blendery
+//
+//  Created by 박영언 on 12/28/25.
+//
+
+import Foundation
+
+enum Temperature {
+    case hot, ice
+}
+
+enum CupSize {
+    case l, xl
+}
